a071ed130e338957f334440c0d429f67549ecdd2
Fg7129 <chefmadalion@gmail.com>
2024-11-27 11:59:14 +0300

    Add files via upload

---
+++ ONLINE CLASSES/QUIZ.js
@@ -0,0 +1,13 @@
+//Write a for loop that prints all the numbers from 1 to 100. However:
+
+//If a number is divisible by 3, print "Fizz" instead of the number.
+//If a number is divisible by 5, print "Buzz" instead of the number.
+//If a number is divisible by both 3 and 5, print "FizzBuzz".
+
+console.log("Counting From 1-100:");
+    for (let i = 0; i <= 100; i++) {
+        console.log (i);
+        if (i /= 3){
+            console.log("Fizz")
+        }
+    }
